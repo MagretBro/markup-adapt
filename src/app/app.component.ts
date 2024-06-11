7c3ee7c668883488499e01201589edce71e35f9d
MagretBro <magretbro@gmail.com>
2024-06-11 19:58:33 +0700

    реализация логики о(n) палиндром

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@
 // import { MatFormFieldModule } from '@angular/material/form-field';
 // import { FormsModule } from '@angular/forms';
 
+import { NgIf } from '@angular/common';
 import {Component} from '@angular/core';
 
 @Component({
@@ -21,5 +22,31 @@ export class AppComponent {
     this.result = prepText === prepText.split('').reverse().join('');
     console.log({result: this.result})
   }
-}
+
+  isPalindrome2() {
+    const prepText = this.inputText.toLowerCase().replace(/[^a-zа-яё]/g, '');
+      console.log(prepText)
+      console.log({prepText})
+
+      let left = 0;
+      let right = prepText.length -1;
+
+      while(left < right) {
+        if (prepText[left] !== prepText[right]) {
+          this.result == false;
+          return;
+        }
+        left++;
+        right--;
+      }
+
+        this.result = true;
+        console.log(prepText);
+        console.log({ result: this.result });
+      }
+      
+    }
+
+
+
 
